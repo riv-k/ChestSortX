570c4a69d58596eeeed08cd8d1db84a64e0c9944
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-11-21 23:08:47 +1300

    save progress

--- src/main/java/com/rvik/chestsortx/ChestSortX.java
+++ src/main/java/com/rvik/chestsortx/ChestSortX.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,5 +17,10 @@ public class ChestSortX extends JavaPlugin implements Listener {
   @EventHandler
   public void onPlayerJoin(PlayerJoinEvent event) {
     event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+  }
+
+  @EventHandler
+  public void onInventoryClose(InventoryCloseEvent event) {
+    event.getPlayer().sendMessage(Component.text("Inventory Closed!"));
   }
 }
